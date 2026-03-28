cd9abe2042edb4852a9a5b8bf97a80b1aa287ffe
seagate2001 <kellens.tom@hotmail.com>
2026-03-28 03:42:05 +0100

    netlify form + layout fix

--- app/components/Contact.vue
+++ app/components/Contact.vue
@@ -1,65 +1,66 @@
 <template>
     <BaseTitle title="Contact" />
-        <div class="flex justify-end items-start">
-                <div class="mt-16 w-full lg:w-[60%]"> <h3 class="text-[18px] text-secondary w-full mb-16">
-                Ready to kickstart my professional journey and bring some innovative ideas to life. If you're looking for a driven front-end developer with an eye for detail, you've come to the right place. Send me a message and let's get to know each other!
-            </h3>
-                    <form
-                name="contact"
-                method="POST"
-                data-netlify="true"
-                class="border-dashed relative border-secondary border p-8 md:p-10 w-full"
-            >
-                <div class="flex flex-col gap-10 md:max-w-[480px] lg:max-w-[580px]">
-        
-                    <div class="flex flex-col md:flex-row gap-10 md:gap-20 w-full">
+        <div class="flex justify-end items-start mt-16">
+            <div class="w-full lg:w-[60%]"> 
+                <h3 class="text-[18px] text-secondary w-full mb-16">
+                    Ready to kickstart my professional journey and bring some innovative ideas to life. If you're looking for a driven front-end developer with an eye for detail, you've come to the right place. Send me a message and let's get to know each other!
+                </h3>
+                <form
+                    name="contact"
+                    netlify
+                    method="POST"
+                    class="border-dashed relative border-secondary border p-8 md:p-10 w-full"
+                >
+                    <div class="flex flex-col gap-10 md:max-w-[480px] lg:max-w-[580px]">
+            
+                        <div class="flex flex-col md:flex-row gap-10 md:gap-20 w-full">
+                            <BaseInput
+                                id="name"
+                                name="name"
+                                label="Name"
+                                class="flex-1"
+                                required
+                            />
+                            <BaseInput
+                                id="email"
+                                name="email"
+                                type="email"
+                                label="Email"
+                                class="flex-1"
+                                required
+                            />
+                        </div>
                         <BaseInput
-                            id="name"
-                            name="name"
-                            label="Name"
-                            class="flex-1"
+                            id="subject"
+                            name="subject"
+                            label="Subject"
+                            class="w-full"
                             required
                         />
                         <BaseInput
-                            id="email"
-                            name="email"
-                            type="email"
-                            label="Email"
-                            class="flex-1"
+                            id="message"
+                            name="message"
+                            label="Message"
+                            type="textarea"
+                            class="w-full"
                             required
                         />
+                        <div class="pt-4">
+                            <BaseButton
+                                type="submit"
+                                text="Send Message"
+                                width="w-full lg:w-3/4"
+                                icon="arrow-right-black.png"
+                            />
+                        </div>
                     </div>
-                    <BaseInput
-                        id="subject"
-                        name="subject"
-                        label="Subject"
-                        class="w-full"
-                        required
-                    />
-                    <BaseInput
-                        id="message"
-                        name="message"
-                        label="Message"
-                        type="textarea"
-                        class="w-full"
-                        required
-                    />
-                    <div class="pt-4">
-                        <BaseButton
-                            type="submit"
-                            text="Send Message"
-                            width="w-full lg:w-3/4"
-                            icon="arrow-right-black.png"
-                        />
-                    </div>
-                </div>
-            </form>
+                </form>
 
-            <a href="mailto:kellens.tom@hotmail.com" class="flex items-center gap-2 my-10 w-fit">
-                <NuxtImg src="/icons/envelope-white.png" width="30px" />
-                <h2 class="font-secondary text-xl">kellens.tom@hotmail.com</h2>
-            </a>
-        </div>
+                <a href="mailto:kellens.tom@hotmail.com" class="flex items-center gap-2 my-10 w-fit">
+                    <NuxtImg class="pointer-events-none" src="/icons/envelope-white.png" width="30px" />
+                    <h2 class="font-secondary text-xl">kellens.tom@hotmail.com</h2>
+                </a>
+            </div>
 
         <NuxtImg 
             class="hidden lg:block" 
